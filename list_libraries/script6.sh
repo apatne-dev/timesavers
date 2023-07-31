bd0727bf8ef374df6ce7a746d7b68892598b3024
apatne-dev <85539477+apatne-dev@users.noreply.github.com>
2023-07-31 12:55:21 +0530

    Create script6.sh

---
+++ list_libraries/script6.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+binary_name="contrail-vrouter-dpdk"
+echo "Getting dependencies for ${binary_name} binary"
+python3 -m venv python3-virtualenv
+source python3-virtualenv/bin/activate
+cd timesavers/list_libraries/
+pip install -r requirements.txt
+./list_libraries_rpm /usr/bin/${binary_name} ${binary_name}.xlsx
